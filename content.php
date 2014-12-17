30d47aa21020b1061b17e96ff56324f273a4442e
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2014-12-17 11:46:39 +0100

    More space for post/page titles

--- content.php
+++ content.php
@@ -1,12 +1,16 @@
 <article class="page-section post">
   <div class="row">
-    <div class="small-11 small-centered medium-7 columns">
+    <div class="small-11 small-centered medium-10 columns">
       <header class="text-center">
         <h2><?php the_title(); ?></h2>
         <?php if (is_single()): ?>
           <span><?php the_date(); ?></span>
         <?php endif ?>
       </header>
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-11 small-centered medium-7 columns">
       <div>
         <?php the_content(); ?>
       </div>
